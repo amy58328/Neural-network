219d9ecbc26c643c2bed1fcf200eae21bd5dcf09
Una <43245005+amy58328@users.noreply.github.com>
2019-12-30 13:37:18 +0800

    Add files via upload

--- program2/3A_3.py
+++ program2/3A_3.py
@@ -3,31 +3,35 @@ import math
 import random
 
 hidden_layer = 1
-number_of_input = 4
-number_of_output = 3
+number_of_input = 2
+number_of_output = 2
 learning_rate = 0.3
 
 # 各種開矩陣
-w1 = np.zeros((hidden_layer,number_of_input),dtype = np.float)
-b1 = np.zeros((hidden_layer,1),dtype = np.float)
+# w1 = np.zeros((hidden_layer,number_of_input),dtype = np.float)
+# b1 = np.zeros((hidden_layer,1),dtype = np.float)
 a1 = np.zeros((hidden_layer,1),dtype = np.float)
 error1 = np.zeros((hidden_layer,1),dtype = np.float)
 
-w2 = np.zeros((number_of_output,hidden_layer),dtype = np.float)
-b2 = np.zeros((number_of_output,1),dtype = np.float)
+# w2 = np.zeros((number_of_output,hidden_layer),dtype = np.float)
+# b2 = np.zeros((number_of_output,1),dtype = np.float)
 a2 = np.zeros((number_of_output,1),dtype = np.float)
 error2 = np.zeros((number_of_output,1),dtype = np.float)
 
 # 各種初始化
-for i in range(0,hidden_layer):
-	for j in range(0,number_of_input):
-		w1[i][j] = random.random()
-	b1[i] = random.random()
+# for i in range(0,hidden_layer):
+# 	for j in range(0,number_of_input):
+# 		w1[i][j] = random.random()
+# 	b1[i] = random.random()
 
-for i in range(0,number_of_output):
-	for j in range(0,hidden_layer):
-		w2[i][j] = random.random()
-	b2[i] = random.random()
+# for i in range(0,number_of_output):
+# 	for j in range(0,hidden_layer):
+# 		w2[i][j] = random.random()
+# 	b2[i] = random.random()
+w1 = np.array([0.1 , 0.1])
+b1 = np.array([0.1])
+w2 = np.array([[0.1],[0.1]])
+b2 = np.array([[0.1],[0.1]])
 
 print("w1:",w1)
 print("b1:",b1)
@@ -35,13 +39,22 @@ print("w2:",w2)
 print("b2:",b2)
 
 # p 跟 t 的設定(讀黨)
-p = np.array([[6],[2.7],[5.1],[1.6]])
-t = np.array([[0.1],[0.9],[0.1]])
+p = np.array([
+	[1.],
+	[0.]
+])
+
+t = np.array([
+	[1.],
+	[0.]
+])
 
 
 # 計算a1
 for i in range(0,hidden_layer):
-	temp = w1[i].dot(p)+b1[i]
+	print("w1[i]",w1[i])
+	print("p",p)
+	temp = w1.dot(p)+b1
 	print("temp",temp)
 	temp = 1 / (1 + math.exp(-temp))
 	a1[i] = temp
@@ -78,11 +91,17 @@ for i in range(0,number_of_output):
 	for j in range(0,hidden_layer):
 		temp = w2[i][j] + 2 * learning_rate * error2[i] * a1[j]
 		w2[i][j] = temp
+	temp = b2[i] + 2 * learning_rate * error2[i]
+	b2[i] = temp
 print("new w2:" , w2)
+print("new b2:" , b2)
 
 for i in range(0,hidden_layer):
 	for j in range(0,number_of_input):
-		temp = w1[i][j] + 2* learning_rate * error1[i] * p[j]
-		w1[i][j] = temp
+		temp = w1[j] + 2* learning_rate * error1 * p[j]
+		w1[j] = temp
+	temp = b1 + 2 * learning_rate * error1
+	b1 = temp
 print("new w1" , w1	)
+print("new b1:" , b1)
 
--- program2/3A_3O.py
+++ program2/3A_3O.py
@@ -2,114 +2,255 @@ import numpy as np
 import math 
 import random
 
-hidden_layer = 1
+hidden = [5,10,15,20]
+learning = [0.3,0.5,0.7,1]
+
+# hidden_layer = 5
 number_of_input = 4
 number_of_output = 3
-learning_rate = 0.3
+# learning_rate = 1
+target_RMSE = 0.15
+MAX_epoch = 50000
 
-# 各種開矩陣
-w1 = np.zeros((hidden_layer,number_of_input),dtype = np.float)
-b1 = np.zeros((hidden_layer,1),dtype = np.float)
-a1 = np.zeros((hidden_layer,1),dtype = np.float)
-error1 = np.zeros((hidden_layer,1),dtype = np.float)
+def backpropogation(hidden_layer,learning_rate):
 
-w2 = np.zeros((number_of_output,hidden_layer),dtype = np.float)
-b2 = np.zeros((number_of_output,1),dtype = np.float)
-a2 = np.zeros((number_of_output,1),dtype = np.float)
-error2 = np.zeros((number_of_output,1),dtype = np.float)
-p = np.array([[6],[2.7],[5.1],[1.6]])
-t = np.array([[0.1],[0.9],[0.1]])
+	print("Number of hidden layer neural : " ,hidden_layer)
+	print("The learning rate : ",learning_rate)
+	# 各種開矩陣
+	w1 = np.zeros((hidden_layer,number_of_input),dtype = np.float)
+	b1 = np.zeros((hidden_layer,1),dtype = np.float)
+	a1 = np.zeros((hidden_layer,1),dtype = np.float)
+	error1 = np.zeros((hidden_layer,1),dtype = np.float)
 
-# 各種初始化
-for i in range(0,hidden_layer):
-	for j in range(0,number_of_input):
-		w1[i][j] = random.random()
-	b1[i] = random.random()
+	w2 = np.zeros((number_of_output,hidden_layer),dtype = np.float)
+	b2 = np.zeros((number_of_output,1),dtype = np.float)
+	a2 = np.zeros((number_of_output,1),dtype = np.float)
+	error2 = np.zeros((number_of_output,1),dtype = np.float)
+	p = np.array([[6],[2.7],[5.1],[1.6]])
+	t = np.array([[0.1],[0.9],[0.1]])
 
-for i in range(0,number_of_output):
-	for j in range(0,hidden_layer):
-		w2[i][j] = random.random()
-	b2[i] = random.random()
-
-print("w1:",w1)
-print("b1:",b1)
-print("w2:",w2)
-print("b2:",b2)
-
-
-
-
-file = open("iris_training_data.txt","r")
-lists = file.readlines() # 以行分開
-datalen = len(lists)
-
-RMSE = 0
-
-for li in lists:
-	# p 跟 t 的設定(讀黨)
-	li = li.strip('\n').split(" " )
-	index = 0
-	for i in li:
-		if(index == 4):
-			if(i == 'versicolor'):
-				t = ([[0.1],[0.9],[0.1]])
-			elif(i == 'virginica'):
-				t = ([[0.1],[0.1],[0.9]])
-			elif(i == 'setosa'):
-				t = ([[0.9],[0.1],[0.1]])
-		elif(index < 4):
-			i = float(i)
-			p[index] = i
-
-		index += 1		
-	print("p",p)
-	print("t",t)
-
-	# 計算a1
+	# 各種初始化
 	for i in range(0,hidden_layer):
-		temp = w1[i].dot(p)+b1[i]
-		print("temp",temp)
-		temp = 1 / (1 + math.exp(-temp))
-		a1[i] = temp
-
-	print("a1:",a1)
-
-	# 計算a2
-	for i in range(0,number_of_output):
-		temp = w2[i].dot(a1) + b2[i]
-		temp = 1 / (1 + math.exp(-temp))
-		a2[i] = temp
-	print("a2:",a2)
-
-	# 計算誤差值
-
-	for i in range(0,number_of_output):
-		temp = (t[i] - a2[i]) * a2[i] * (1 - a2[i])
-		error2[i] = temp
-
-	print("error2:" , error2 )
-
-	for i in range(0,hidden_layer):
-		temp = 0
-		for j in range(0,number_of_output):
-			temp += w2[j][i] * error2[j]
-		temp = temp * a1[i] * (1 - a1[i])
-		error1[i] = temp
-
-	print("error1",error1)
-
-	# 更新的部分
+		for j in range(0,number_of_input):
+			w1[i][j] = random.random()
+		b1[i] = random.random()
 
 	for i in range(0,number_of_output):
 		for j in range(0,hidden_layer):
-			temp = w2[i][j] + 2 * learning_rate * error2[i] * a1[j]
-			w2[i][j] = temp
-	print("new w2:" , w2)
+			w2[i][j] = random.random()
+		b2[i] = random.random()
 
-	for i in range(0,hidden_layer):
-		for j in range(0,number_of_input):
-			temp = w1[i][j] + 2* learning_rate * error1[i] * p[j]
-			w1[i][j] = temp
-	print("new w1" , w1	)
+	print("The init w1:",w1)
+	print("The init b1:",b1)
+	print("The init w2:",w2)
+	print("The init b2:",b2)
 
-	RMSE += sqrt()
+
+	RMSE = 0.5
+	min_RMSE = 1
+	epoch = 0
+
+	while(RMSE > target_RMSE and epoch < MAX_epoch):
+		epoch += 1
+		file = open("iris_training_data.txt","r")
+		lists = file.readlines() # 以行分開
+		datalen = len(lists)
+
+		RMSE = 0.
+
+
+		for li in lists:
+			# p 跟 t 的設定(讀黨)
+			li = li.strip('\n').split(" " )
+			index = 0
+			for i in li:
+				if(index == 4):
+					if(i == 'versicolor'):
+						t = ([[0.1],[0.9],[0.1]])
+					elif(i == 'virginica'):
+						t = ([[0.1],[0.1],[0.9]])
+					elif(i == 'setosa'):
+						t = ([[0.9],[0.1],[0.1]])
+				elif(index < 4):
+					i = float(i)
+					p[index] = i
+
+				index += 1		
+
+			# 計算a1
+			for i in range(0,hidden_layer):
+				temp = w1[i].dot(p)+b1[i]
+				temp = 1 / (1 + math.exp(-temp))
+				a1[i] = temp
+
+
+			# 計算a2
+			for i in range(0,number_of_output):
+				temp = w2[i].dot(a1) + b2[i]
+				temp = 1 / (1 + math.exp(-temp))
+				a2[i] = temp
+
+			# 計算誤差值 & 計算RMSE
+			RMSE_temp = 0.
+			for i in range(0,number_of_output):
+				temp = (t[i] - a2[i]) * a2[i] * (1 - a2[i])
+				error2[i] = temp
+
+				RMSE_temp += pow((t[i] - a2[i]),2)
+
+			for i in range(0,hidden_layer):
+				temp = 0
+				for j in range(0,number_of_output):
+					temp += w2[j][i] * error2[j]
+				temp = temp * a1[i] * (1 - a1[i])
+				error1[i] = temp
+
+			# 更新的部分
+
+			for i in range(0,number_of_output):
+				for j in range(0,hidden_layer):
+					temp = w2[i][j] + 2 * learning_rate * error2[i] * a1[j]
+					w2[i][j] = temp
+				temp = b2[i] + 2 * learning_rate * error2[i]
+				b2[i] = temp
+
+			for i in range(0,hidden_layer):
+				for j in range(0,number_of_input):
+					temp = w1[i][j] + 2* learning_rate * error1[i] * p[j]
+					w1[i][j] = temp
+				temp = b1[i] + 2 * learning_rate * error1[i]
+				b1[i] = temp
+
+			RMSE_temp /= 120
+			RMSE += math.sqrt(RMSE_temp)
+
+			if(RMSE < min_RMSE):
+				min_RMSE = RMSE
+				ans_w1 = w1
+				ans_b1 = b1
+				ans_b2 = b2
+				ans_w2 = w2
+
+	print("The training epoch : ", epoch)
+
+	print("The finial w1 : ",ans_w1)
+	print("The finial b1 : ",ans_b1)
+	print("The finial w2 : ",ans_w2)
+	print("The finial b2 : ",ans_b2)
+	correct = 0
+
+	# 計算 錯誤率
+	file = open("iris_training_data.txt","r")
+	lists = file.readlines() # 以行分開
+	datalen = len(lists)
+
+	for li in lists:
+		# p 跟 t 的設定(讀黨)
+		li = li.strip('\n').split(" " )
+		index = 0
+		for i in li:
+			if(index == 4):
+				if(i == 'versicolor'):
+					t = ([[0.1],[0.9],[0.1]])
+				elif(i == 'virginica'):
+					t = ([[0.1],[0.1],[0.9]])
+				elif(i == 'setosa'):
+					t = ([[0.9],[0.1],[0.1]])
+			elif(index < 4):
+				i = float(i)
+				p[index] = i
+
+			index += 1	
+
+		# 計算a1
+		for i in range(0,hidden_layer):
+				temp = w1[i].dot(p)+b1[i]
+				temp = 1 / (1 + math.exp(-temp))
+				a1[i] = temp
+
+		# 計算a2
+		for i in range(0,number_of_output):
+			temp = w2[i].dot(a1) + b2[i]
+			temp = 1 / (1 + math.exp(-temp))
+			a2[i] = temp
+
+		max_index = 0
+
+		for i in range(0,number_of_output):
+			if(a2[i] > a2[max_index]):
+				max_index = i
+		
+		if(max_index == 0):
+			a = np.array([[0.9],[0.1],[0.1]])
+		elif(max_index == 1):
+			a = np.array([[0.1],[0.9],[0.1]])
+		elif(max_index == 2):
+			a = np.array([[0.1],[0.1],[0.9]])
+
+		if((a == t).all()):
+			correct += 1
+
+	print("The Correct rate of training data : ",correct/120*100)
+
+
+	correct = 0
+	file = open("iris_testing_data.txt","r")
+	lists = file.readlines() # 以行分開
+	datalen = len(lists)
+
+	for li in lists:
+		# p 跟 t 的設定(讀黨)
+		li = li.strip('\n').split(" " )
+		index = 0
+		for i in li:
+			if(index == 4):
+				if(i == 'versicolor'):
+					t = ([[0.1],[0.9],[0.1]])
+				elif(i == 'virginica'):
+					t = ([[0.1],[0.1],[0.9]])
+				elif(i == 'setosa'):
+					t = ([[0.9],[0.1],[0.1]])
+			elif(index < 4):
+				i = float(i)
+				p[index] = i
+
+			index += 1	
+
+		# 計算a1
+		for i in range(0,hidden_layer):
+				temp = w1[i].dot(p)+b1[i]
+				temp = 1 / (1 + math.exp(-temp))
+				a1[i] = temp
+
+		# 計算a2
+		for i in range(0,number_of_output):
+			temp = w2[i].dot(a1) + b2[i]
+			temp = 1 / (1 + math.exp(-temp))
+			a2[i] = temp
+
+		max_index = 0
+
+		for i in range(0,number_of_output):
+			if(a2[i] > a2[max_index]):
+				max_index = i
+		
+		if(max_index == 0):
+			a = np.array([[0.9],[0.1],[0.1]])
+		elif(max_index == 1):
+			a = np.array([[0.1],[0.9],[0.1]])
+		elif(max_index == 2):
+			a = np.array([[0.1],[0.1],[0.9]])
+
+		if((a == t).all()):
+			correct += 1
+
+	print("The Correct rate of testing data : ",correct/30*100)
+
+for q in range(0,4):
+	for w in range(0,4):
+		print("=========================================")
+		backpropogation(hidden[q],learning[w])
+		print("=========================================")
+
+
+	
